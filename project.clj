dc0f491d6345bf0db898139d95d05322edaedbc7
Hnatekmar <m.hnatma@gmail.com>
2018-06-09 20:23:36 +0200

    Empty project skeleton

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject different "0.1.0-SNAPSHOT"
+  :description "Simple library for symbolic differentiation"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
